84ddfed8fa1a443daeca1aebef98e0da851652f5
jaden <jaden.otalvaroq@utp.edu.co>
2026-03-20 08:16:31 -0500

    base de datos en atlas

--- db/client.py
+++ db/client.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local
+db_client = MongoClient('mongodb+srv://jaden:derogi2002@cluster0.mpghdmt.mongodb.net/?appName=Cluster0').ferreteria_db
